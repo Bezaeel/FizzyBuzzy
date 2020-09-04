cff9fcaf401a34bce0a3ea96573b18224bca6d5c
Bezaeel <talabi.rotimi@hotmail.com>
2020-09-04 02:29:46 +0100

    Add project files.

---
+++ FizzyBuzzy/FizzyBuzzy.Service/Services/FizzBuzz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzyBuzzy.Service.Services
+{
+    public class FizzBuzz : IFizzBuzz
+    {
+        public async Task<string> GenerateFizzBuzz(int value)
+        {
+            if((value%3) == 0)
+            {
+                
+            }
+        }
+    }
+}
